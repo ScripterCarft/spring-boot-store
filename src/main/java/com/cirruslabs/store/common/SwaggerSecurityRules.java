8d31987f5dc46d9e6b3a978cdf0fd0250d46a50b
BuildTools <unconfigured@null.spigotmc.org>
2025-05-03 23:09:59 +0200

    Actuator

--- src/main/java/com/cirruslabs/store/common/SwaggerSecurityRules.java
+++ src/main/java/com/cirruslabs/store/common/SwaggerSecurityRules.java
@@ -10,6 +10,7 @@ public class SwaggerSecurityRules implements SecurityRules {
     public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
         registry.requestMatchers("/swagger-ui/**").permitAll()
                 .requestMatchers("/swagger-ui.html/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll();
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers("/actuator/health/**").permitAll();
     }
 }
